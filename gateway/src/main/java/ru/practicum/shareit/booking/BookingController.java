bdaef22a8fd16e03ead78c073cb289930ff1d31b
DeveloperJavaMain <115426650+DeveloperJavaMain@users.noreply.github.com>
2023-06-28 23:31:46 +0300

    Update BookingController.java

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.shareit.booking.dto.BookingDtoPost;
 import ru.practicum.shareit.booking.dto.State;
+import ru.practicum.shareit.exceptions.StatusException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -35,40 +36,49 @@ public class BookingController {
 
     @PatchMapping("/{itemId}")
     public ResponseEntity<Object> approve(@PathVariable long itemId,
-                              @RequestParam Boolean approved,
-                              @RequestHeader("X-Sharer-User-Id") Long userId) {
+                                          @RequestParam Boolean approved,
+                                          @RequestHeader("X-Sharer-User-Id") Long userId) {
         log.info("PATCH /bookings/{} {}", itemId, approved);
         return client.approve(itemId, userId, approved);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getById(@PathVariable long id,
-                              @RequestHeader("X-Sharer-User-Id") Long userId) {
+                                          @RequestHeader("X-Sharer-User-Id") Long userId) {
         log.info("GET /bookings/{}", id);
         return client.getById(id, userId);
     }
 
     @GetMapping
     public ResponseEntity<Object> getList(@RequestParam(defaultValue = "ALL") String state,
-                                    @RequestHeader("X-Sharer-User-Id") Long userId,
-                                    @RequestParam(defaultValue = "0")
-                                    @Min(0) int from,
-                                    @RequestParam(defaultValue = "10")
-                                    @Min(0) int size) {
+                                          @RequestHeader("X-Sharer-User-Id") Long userId,
+                                          @RequestParam(defaultValue = "0")
+                                          @Min(0) int from,
+                                          @RequestParam(defaultValue = "10")
+                                          @Min(0) int size) {
         log.info("GET /bookings/state={}", state);
-        State tstate = State.valueOf(state);
-        return client.getListByBooker(userId, tstate, from, size);
+        try {
+            State _state = State.valueOf(state);
+            return client.getListByBooker(userId, _state, from, size);
+        } catch (IllegalArgumentException e) {
+            throw new StatusException("Unknown state: " + state);
+        }
     }
 
     @GetMapping("/owner")
     public ResponseEntity<Object> getListByOwner(@RequestParam(defaultValue = "ALL") String state,
-                                           @RequestHeader("X-Sharer-User-Id") Long userId,
-                                           @RequestParam(defaultValue = "0")
-                                           @Min(0) int from,
-                                           @RequestParam(defaultValue = "10")
-                                           @Min(0) int size) {
+                                                 @RequestHeader("X-Sharer-User-Id") Long userId,
+                                                 @RequestParam(defaultValue = "0")
+                                                 @Min(0) int from,
+                                                 @RequestParam(defaultValue = "10")
+                                                 @Min(0) int size) {
         log.info("GET /bookings/state={}", state);
-        State tstate = State.valueOf(state);
-        return client.getListByOwner(userId, tstate, from, size);
+        try {
+            State _state = State.valueOf(state);
+            return client.getListByOwner(userId, _state, from, size);
+        } catch (IllegalArgumentException e) {
+            throw new StatusException("Unknown state: " + state);
+        }
+
     }
 }
